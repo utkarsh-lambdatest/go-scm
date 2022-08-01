4aaa6cdbba89925c124474d67fac4a3f377cc8e3
Utkarsh Gupta <utkarshg@lambdatest.com>
2022-08-01 12:58:25 +0530

    added check langugae support functionality in repo gitlab

--- scm/driver/gitlab/repo.go
+++ scm/driver/gitlab/repo.go
@@ -98,8 +98,11 @@ func (s *repositoryService) List2(ctx context.Context, orgSlug string, opts scm.
 	return nil, nil, scm.ErrNotSupported
 }
 
-func (s *repositoryService) ListRepoLanguages(context.Context, string) (map[string]float64, *scm.Response, error) {
-	return nil, nil, scm.ErrNotSupported
+func (s *repositoryService) ListRepoLanguages(ctx context.Context, repo string) (map[string]float64, *scm.Response, error) {
+	path := fmt.Sprintf("api/v4/projects/%s/languages", encode(repo))
+	out := map[string]float64{}
+	res, err := s.client.do(ctx, "GET", path, nil, out)
+	return out, res, err
 }
 
 func (s *repositoryService) ListHooks(ctx context.Context, repo string, opts scm.ListOptions) ([]*scm.Hook, *scm.Response, error) {
